9f9b487916fb5072d520cba6eac8746da4b6b3cb
Mahesh <mahesh.patil388@gmail.com>
2025-07-06 17:01:20 +0530

    edit done

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello from Python + Jenkins!")
+
+n=input(("Enter the numbers:")).strip()
+t=1
+if n.isdigit():
+    for i in n:
+     t=t*int(i)
+    print(f"The product is {t}")
+else:
+   print("Please enter only digits")
