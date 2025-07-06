1b45070f7e67d04cf8bc03f94a9e62eaf4058d9d
Mahesh <mahesh.patil388@gmail.com>
2025-07-06 17:10:48 +0530

    sum is doen

--- hello.py
+++ hello.py
@@ -7,3 +7,13 @@ if n.isdigit():
     print(f"The product is {t}")
 else:
    print("Please enter only digits")
+
+
+n=input("enter the number only:").strip()
+t=0
+if n.isdigit():
+    for i in n:
+        t=t+int(i)
+    print(f"The sum is {t}")
+else:
+    print("u entered wrong numbers")
