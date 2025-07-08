f52f300486bbe956de59bd0c5720a7df09547013
Mahesh <mahesh.patil388@gmail.com>
2025-07-08 07:53:37 +0530

    property decorator

--- work/oop.py
+++ work/oop.py
@@ -3,14 +3,31 @@
 # Can access or modify class-level state.
 # Marked with @classmethod.
 
-class Company():
-    emp="mahesh"
+# class Company():
+#     emp="mahesh"
     
-    @classmethod
-    def change_name(cls,new_name):
-        # cls.emp=new_name
-        print(f"new name is {new_name}")
+#     @classmethod
+#     def change_name(cls,new_name):
+#         # cls.emp=new_name
+#         print(f"new name is {new_name}")
 
-Company.change_name("Sagar")
-Company.change_name("map")
+# Company.change_name("Sagar")
+# Company.change_name("map")
 
+
+
+class Comp:
+    def __init__(self,radius):
+        self._radius=radius
+
+    @property
+    def area(self):
+         return 3.14 * self._radius **2
+    @area.setter 
+    def set_area(self,value):
+        self._radius=value
+c=Comp(2)
+print(c.area)
+c.set_area=3
+print(c.area)
+ 
